bdcdbd4f21356cfe533ec717950ef33f116bc4c9
Emiliano Gaytan <cfphpflex@gmail.com>
2022-10-24 17:21:27 -0700

    save endoints

---
+++ src/nodeapi/getprofile.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    id: 1,
+    fname: "FirstName LastName",
+    waist: 30,
+    length: 32,
+  }
+
--- src/nodeapi/routes.js
+++ src/nodeapi/routes.js
@@ -1,10 +1,43 @@
 const express = require('express');
-const todos = require("./todos");
+const todos                 = require("./todos");
+const getallpc9             = require("./getallpc9");
+const getallpc9matchsearch  = require("./getallpc9matchsearch");
+const getallpc9matchprofile = require("./getallpc9matchprofile");
+const getprofile            = require("./getprofile");
+const saveprofile           = require("./saveprofile");
 
 const router = express.Router();
 
-router.get("/todos", function(req, res) {
+router.get("/gettodos", function(req, res) {
   res.json(todos);
 });
+
+router.get("/getallpc9", function(req, res) {
+  res.json(getallpc9);
+});
+
+router.get("/getallpc9matchsearch", function(req, res) {
+  res.json(getallpc9);
+});
+
+
+router.get("/getallpc9matchprofile", function(req, res) {
+  res.json(getallpc9);
+});
+
+
+
+router.get("/getprofile", function(req, res) {
+  res.json(getprofile);
+});
+
+router.get("/saveprofile", function(req, res) {
+  res.json(saveprofile);
+});
+
+router.get("/updateprofile", function(req, res) {
+  res.json(updateprofile);
+});
+
 
 module.exports = router;
