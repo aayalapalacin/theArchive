2cfccb6bc0fe713d177efa9a9e36631aefeeeb91
avokeys <alexander.ayala.palacin@gmail.com>
2022-10-27 04:02:41 +0000

    finished pc9Input component

--- src/front/js/pages/pc9Input.js
+++ src/front/js/pages/pc9Input.js
@@ -1,37 +1,74 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Formik, Form } from "formik";
+import TextField from "../component/textField";
+import * as Yup from "yup";
+import pc9Img from "../../img/pc9_2.png";
 import "../../styles/pc9Input.css";
+
+const validate = Yup.object({
+  pc9Input: Yup.string()
+    .required("This field is required")
+    .max(9, "must be 9 characters long")
+    .min(9, "must be 9 characters long"),
+});
 
 function Pc9Input() {
   return (
-    <div>
-      <div className="row archiveBackBtn">
-        <div className="col-1">
-          <FontAwesomeIcon
-            icon={faChevronLeft}
-            className="chevron-left"
-          ></FontAwesomeIcon>
+    <Formik
+      initialValues={{
+        pc9Input: "",
+      }}
+      validationSchema={validate}
+    >
+      {(formik) => (
+        <div>
+          <div className="row archiveBackBtn">
+            <div className="col-1">
+              <FontAwesomeIcon
+                icon={faChevronLeft}
+                className="chevron-left my-2"
+              ></FontAwesomeIcon>
+            </div>
+            <div className="col-11">
+              <p className="archiveBackTitle my-2">the Archive</p>
+            </div>
+          </div>
+          <div className="row myLeviJeanTitle mx-auto mt-4">
+            <span className="MY-LEVIS-JEANS">MY LEVI’S JEANS</span>
+          </div>
+          <div className="row 2of2">
+            <span className="-of-2 mx-auto mt-2">2 of 2</span>
+          </div>
+          <div className="row pc9Instructions">
+            <span className="Input-the-pc9-code-serial-number-found-on-the-back-of-the-care-tag mx-auto">
+              Input the pc9 code (serial number) found on the back of the care
+              tag.
+            </span>
+          </div>
+          <div className="row pc9Img">
+            <img className="mb-5" src={pc9Img} alt="pc9 tag" />
+          </div>
+          <div className="row pc9Input">
+            <div>
+              <TextField
+                className="pc9InputField ms-2"
+                label="PC9"
+                id="pc9Input"
+                name="pc9Input"
+                type="text"
+              />
+            </div>
+          </div>
+          <div className="row submit">
+            <button className=" mx-auto mt-5 submitBtn" type="submit">
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="col-11">
-          <p className="archiveBackTitle">the Archive</p>
-        </div>
-      </div>
-      <div className="row myLeviJeanTitle">
-        <span class="MY-LEVIS-JEANS">MY LEVI’S JEANS</span>
-      </div>
-      <div className="row 2of2">
-        <span class="-of-2">2 of 2</span>
-      </div>
-      <div className="row pc9Instructions">
-        <span class="Input-the-pc9-code-serial-number-found-on-the-back-of-the-care-tag">
-          Input the pc9 code (serial number) found on the back of the care tag.
-        </span>
-      </div>
-      <div className="row pc9Img"></div>
-      <div className="row pc9Input"></div>
-      <div className="row submit"></div>
-    </div>
+      )}
+    </Formik>
   );
 }
 
