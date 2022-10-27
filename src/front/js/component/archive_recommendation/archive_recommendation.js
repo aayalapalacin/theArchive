73deaf62e2c5565338e06422a8f021d7d0297765
David Domingo <ddomingo824@gmail.com>
2022-10-27 21:55:37 +0000

    updated database function

--- src/front/js/component/archive_recommendation/archive_recommendation.js
+++ src/front/js/component/archive_recommendation/archive_recommendation.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ArchiveMatch, MatchPc9 } from '../../../util/levi_datatbase_util'
+import ArchiveHeader from './archive_header'
 import PageNavigation from './page_navigation'
 import Pc9Result from './pc9_result'
 
 import RecommendationList from './recommendation_list'
 import RetiredFavorite from './retired_favorite'
 
-const ArchiveRecommendation = ({jean}) => {
+const ArchiveRecommendation = ({pc9}) => {
+    const [matchingJean, setMatchingJean] = useState(MatchPc9(pc9))
+
+    // useEffect( () => {
+    //     console.log('archiveRecommendations')
+    //     console.log(archiveRecommendations)
+    // }, [archiveRecommendations])
+
     return(
         <div>
-            <Pc9Result />
+            <ArchiveHeader />
+            <Pc9Result matchingJean={matchingJean} />
             <RetiredFavorite />
-            <RecommendationList />
+            <RecommendationList pc9={pc9}/>
             <PageNavigation />
         </div>
     )
